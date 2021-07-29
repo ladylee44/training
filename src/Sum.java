f0b710489670825808c0c46dbf90dd34b93a8001
lamlt1211 <lamchuot0@gmail.com>
2021-07-29 23:09:15 +0700

    sum

---
+++ src/Sum.java
@@ -0,0 +1,20 @@
+public class Sum {
+    public static void sum() {
+        int a = 5, b = 6;
+        int sum;
+        sum = a + b;
+        System.out.println("Tong 2 so la " + sum);
+    }
+
+    public static void binhphuong() {
+        int c = 6;
+        int binhphuong;
+        binhphuong = c * c;
+        System.out.println("Binh phuong la " + binhphuong);
+    }
+
+    public static void main(String[] args) {
+        sum();
+        binhphuong();
+    }
+}
